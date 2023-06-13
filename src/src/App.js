7292d57f75ffbfd0fed0b0c975686336236e7946
Roman Bakirov <bakirov.important@gmail.com>
2023-06-13 13:43:11 +0600

    1.5.11. Item user select no.

--- src/src/App.js
+++ src/src/App.js
@@ -31,7 +31,7 @@ import ItemContext from "./components/ItemContext";
 
 document.title = 'BRO Launcher';
 
-const appVersion = '1.5.10';
+const appVersion = '1.5.11';
 
 const addNewBookmark = () => () => {
     alert('add new modal window here');
