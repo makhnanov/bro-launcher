71421ae780e07c29da158b97d9f8f6ba78ff289a
Roman Bakirov <bakirov.important@gmail.com>
2023-06-18 01:50:28 +0600

    Add preview feature.

--- src/src/App.js
+++ src/src/App.js
@@ -1,30 +1,13 @@
-import Outlook from './img/Outlook.svg';
-import Confluence from './img/Confluence.svg';
-import ChatGPT from './img/ChatGPT.png';
-import Jira from './img/Jira.png';
-import WebStorm from './img/WebStorm.png';
-import PhpStorm from './img/PhpStorm.png';
-import Diary from './img/Diary.webp';
-import Bitbucket from './img/Bitbucket.svg';
-import KeePassXC from './img/KeePassXC.png';
-import Firefox from './img/Firefox.png';
-import Mousepad from './img/Mousepad.png';
-import Zoom from './img/Zoom.svg';
-import Postman from './img/Postman.svg';
-import Without from './img/Without.jpg';
-import YouTube from './img/YouTube.png';
-import Calculator from './img/Calculator.png';
-import Tinder from './img/Tinder.svg';
-import Github from './img/Github.svg';
-import Plus from './img/Plus.svg';
-import GTranslate from './img/GTranslate.png';
-import Telegram from './img/Telegram.png';
-import Cross from './img/cross-mark-svgrepo-com.svg';
-
-import './App.css';
+import * as img from "./images"
+import "./App.css";
+import "./description.css";
 import Modal from "react-overlays/Modal";
 import React, {useState, useRef, useEffect} from "react";
 import ItemContext from "./components/ItemContext";
+
+import Voice1 from "./voice/1.ogg";
+import Voice2 from "./voice/2.ogg";
+import Voice3 from "./voice/3.ogg";
 
 document.title = 'BRO Launcher';
 
@@ -169,7 +152,7 @@ function App() {
                 let domain = (new URL(newBookmarkLink)).hostname.replace('www.', '');
                 image = 'https://www.google.com/s2/favicons?domain=' + domain + '&sz=128';
             } catch (error) {
-                image = Without;
+                image = img.Without;
             }
         }
         let text = newBookmarkText;
@@ -262,163 +245,204 @@ function App() {
 
     const useFocus = () => {
         const htmlElRef = useRef(null)
-        const setFocus = () => {htmlElRef.current && htmlElRef.current.focus()}
-        return [ htmlElRef, setFocus ]
+        const setFocus = () => {
+            htmlElRef.current && htmlElRef.current.focus()
+        }
+        return [htmlElRef, setFocus]
     }
 
     const [inputLinkRef, setInputFocus] = useFocus();
     const [inputTextRef, setTextFocus] = useFocus();
+
 
     function needShowModal() {
         setIndexForEdit(null);
         setShowModal(true);
     }
 
+    let voices = [Voice1, Voice2, Voice3];
+    let latestIndex = -1;
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    let asHours = true;
+
+    const playTagline = (e) => {
+        e.target.classList.toggle("voice-fff_on")
+        e.target.style.transition = "all ." + getRandomInt(3, 6) + "s ease-in-out";
+        if (e.target.style.transform) {
+            e.target.style.removeProperty("transform");
+        } else {
+            e.target.style.transform = "rotate(" + (asHours ? "" : "-") + getRandomInt(90, 360) + "deg)" + " " + "scale(1.3)";
+            asHours = !asHours
+        }
+        if (typeof voices[++latestIndex] === "undefined") {
+            latestIndex = 0
+        }
+        let audio = new Audio(voices[latestIndex]);
+        audio.play();
+    }
+
     return (
-        <div className="App"
-             onClick={(e) => {
-                 unLockScreen(e)
-             }}
-             style={{filter: !isActive ? 'none' : '', backgroundColor: !isActive ? 'rgba(0, 0, 0, 0)' : ''}}
-        >
-            <div className="App-header" style={{opacity: !isActive ? '0' : '1'}}>
+        <div>
+            <audio src={Voice1}></audio>
+            <div className={"site-description"}>
+                <div className={"site-description__bro-launcher"}>
+                    <span className={"site-description__bro"}>BRO</span> Launcher
+                </div>
+                <div className={"site-description__tagline"}>
+                    <div className={"description__tagline-text"}>One tap instead of three</div>
+                    <img className={"voice-fff"} src={img.Voice} alt={"Voice"} onClick={playTagline}/>
+                </div>
+            </div>
+            <div className="App"
+                 onClick={(e) => unLockScreen(e)}
+                 style={{filter: !isActive ? 'none' : '', backgroundColor: !isActive ? 'rgba(0, 0, 0, 0)' : ''}}>
+                <div className="App-header" style={{opacity: !isActive ? '0' : '1'}}>
 
-                <div className={`item-max-header ${menuHidden ? 'menu-hidden' : ''}`}>
+                    <div className={`item-max-header ${menuHidden ? 'menu-hidden' : ''}`}>
 
-                    <div className={'menu-item close-menu-button'} style={{backgroundImage: `url(${Cross})`}} onClick={toggleHideMenu}>
-                    </div>
+                        <div className={'menu-item close-menu-button'} style={{backgroundImage: `url(${img.Cross})`}}
+                             onClick={toggleHideMenu}>
+                        </div>
 
-                    <div className="launcher">
+                        <div className="launcher">
                         <span className={'bro-three-letters'} onClick={toggleHideMenu}>
                             BRO
                         </span>
-                        <span className={'launcher-text menu-item'}>
+                            <span className={'launcher-text menu-item'}>
                             Launcher
                         </span>
-                        <span className="logo-version-text menu-item"
-                              onClick={() => {
-                                  window.open(localProjectPathForWebStorm)
-                              }}>
+                            <span className="logo-version-text menu-item"
+                                  onClick={() => {
+                                      window.open(localProjectPathForWebStorm)
+                                  }}>
                             v.{appVersion}
                         </span>
-                    </div>
-
-                    <div className={'menu-item'} onClick={exportData}>
-                        Backup
-                    </div>
-
-                    <div className={'menu-item'}>
-                        <label className="custom-file-upload">
-                            <input type="file" className="import-button" onChange={importData}/>
-                            Import
-                        </label>
-                    </div>
-
-                    <div className={'menu-item'} onClick={lockScreen}>
-                        Lock
-                    </div>
-
-                    {/*<div className="">*/}
-                    {/*    Settings*/}
-                    {/*</div>*/}
-
-                    {/*<div className="">*/}
-                    {/*    Login*/}
-                    {/*</div>*/}
-
-                </div>
-
-                <ItemContext bookmarks={bookmarks}
-                             showModal={needShowModal}
-                             showModalForEdit={needShowModalForEdit}
-                             dataVersion={dataVersion}
-                             setDataVersion={setDataVersion}
-                             exportData={exportData}
-                             isMenuHidden={menuHidden}/>
-
-                <div className="item-max">
-                    <div className="y-wrapper" onClick={() => window.open('https://youtube.com/', '_parent')}>
-                        <img src={YouTube} className="y-img" alt="YouTube" width="200" height="50"></img>
-                    </div>
-                    <div className="youtube-search-container">
-                        <input type="text"
-                               className="youtube-search"
-                               onKeyDown={handleYoutubeKeyDown}/>
-                    </div>
-                </div>
-
-                <Modal
-                    className="modal"
-                    show={showModal}
-                    onHide={handleClose}
-                    renderBackdrop={renderBackdrop}
-                    onShow={() => !newBookmarkText ? inputLinkRef.current.focus() : inputTextRef.current.focus()}
-                    onEnter={handleSuccess}
-                >
-                    <div>
-
-                        <div className="modal-header">
-                            <div className="modal-title">Add New Bookmark</div>
                         </div>
 
-                        <div className="modal-desc">
+                        <div className={'menu-item'} onClick={exportData}>
+                            Backup
+                        </div>
 
-                            <p>Text:</p>
+                        <div className={'menu-item'}>
+                            <label className="custom-file-upload">
+                                <input type="file" className="import-button" onChange={importData}/>
+                                Import
+                            </label>
+                        </div>
+
+                        <div className={'menu-item'} onClick={lockScreen}>
+                            Lock
+                        </div>
+
+                        {/*<div className="">*/}
+                        {/*    Settings*/}
+                        {/*</div>*/}
+
+                        {/*<div className="">*/}
+                        {/*    Login*/}
+                        {/*</div>*/}
+
+                    </div>
+
+                    <ItemContext bookmarks={bookmarks}
+                                 showModal={needShowModal}
+                                 showModalForEdit={needShowModalForEdit}
+                                 dataVersion={dataVersion}
+                                 setDataVersion={setDataVersion}
+                                 exportData={exportData}
+                                 isMenuHidden={menuHidden}/>
+
+                    <div className="item-max">
+                        <div className="y-wrapper" onClick={() => window.open('https://youtube.com/', '_parent')}>
+                            <img src={img.YouTube} className="y-img" alt="YouTube" width="200" height="50"></img>
+                        </div>
+                        <div className="youtube-search-container">
                             <input type="text"
-                                   className="input-style"
-                                   ref={inputTextRef}
-                                   onChange={handleBookmarkTextChange}
-                                   onFocus={(e) => e.target.select()}
-                                   value={newBookmarkText}
-                                   onKeyUp={ (e) => e.keyCode === 13 ? handleSuccess() : null}
-                            />
+                                   className="youtube-search"
+                                   onKeyDown={handleYoutubeKeyDown}/>
+                        </div>
+                    </div>
 
-                            <p>Link:</p>
-                            {/*or AnyProgram executor*/}
-                            <input type="text"
-                                   className="input-style"
-                                   ref={inputLinkRef}
-                                   onChange={handleBookmarkLinkChange}
-                                   onKeyUp={ (e) => e.keyCode === 13 ? handleSuccess() : null}
-                                   value={newBookmarkLink}
-                            />
+                    <Modal
+                        className="modal"
+                        show={showModal}
+                        onHide={handleClose}
+                        renderBackdrop={renderBackdrop}
+                        onShow={() => !newBookmarkText ? inputLinkRef.current.focus() : inputTextRef.current.focus()}
+                        onEnter={handleSuccess}
+                    >
+                        <div>
 
-                            <p>Image:</p>
-                            <input type="text" className="input-style" onChange={handleBookmarkImageChange}
-                                   value={newBookmarkImage}/>
+                            <div className="modal-header">
+                                <div className="modal-title">Add New Bookmark</div>
+                            </div>
 
-                            <div className="image-style-selector-block">
-                                <label htmlFor="image-style">Round Image:</label>
-                                <div className="input-style">
-                                    <select name="image-style" id="image-style-id" className="input-style"
-                                            onChange={handleBookmarkImageStyleChange} value={newBookmarkImageStyle ?? "round-image-30"}>
-                                        <option value="none">None</option>
-                                        <option value="round-image-5">5%</option>
-                                        <option value="round-image-10">10%</option>
-                                        <option value="round-image-15">15%</option>
-                                        <option value="round-image-20">20%</option>
-                                        <option value="round-image-25">25%</option>
-                                        <option value="round-image-30">30%</option>
-                                        <option value="round-image-40">40%</option>
-                                        <option value="round-image-50">50%</option>
-                                    </select>
+                            <div className="modal-desc">
+
+                                <p>Text:</p>
+                                <input type="text"
+                                       className="input-style"
+                                       ref={inputTextRef}
+                                       onChange={handleBookmarkTextChange}
+                                       onFocus={(e) => e.target.select()}
+                                       value={newBookmarkText}
+                                       onKeyUp={(e) => e.keyCode === 13 ? handleSuccess() : null}
+                                />
+
+                                <p>Link:</p>
+                                {/*or AnyProgram executor*/}
+                                <input type="text"
+                                       className="input-style"
+                                       ref={inputLinkRef}
+                                       onChange={handleBookmarkLinkChange}
+                                       onKeyUp={(e) => e.keyCode === 13 ? handleSuccess() : null}
+                                       value={newBookmarkLink}
+                                />
+
+                                <p>Image:</p>
+                                <input type="text" className="input-style" onChange={handleBookmarkImageChange}
+                                       value={newBookmarkImage}/>
+
+                                <div className="image-style-selector-block">
+                                    <label htmlFor="image-style">Round Image:</label>
+                                    <div className="input-style">
+                                        <select name="image-style" id="image-style-id" className="input-style"
+                                                onChange={handleBookmarkImageStyleChange}
+                                                value={newBookmarkImageStyle ?? "round-image-30"}>
+                                            <option value="none">None</option>
+                                            <option value="round-image-5">5%</option>
+                                            <option value="round-image-10">10%</option>
+                                            <option value="round-image-15">15%</option>
+                                            <option value="round-image-20">20%</option>
+                                            <option value="round-image-25">25%</option>
+                                            <option value="round-image-30">30%</option>
+                                            <option value="round-image-40">40%</option>
+                                            <option value="round-image-50">50%</option>
+                                        </select>
+                                    </div>
                                 </div>
+
+                            </div>
+
+                            <div className="modal-footer">
+                                <button className="secondary-button" onClick={handleClose}>
+                                    Close
+                                </button>
+                                <button className="primary-button" onClick={handleSuccess}>
+                                    Save Changes
+                                </button>
                             </div>
 
                         </div>
+                    </Modal>
 
-                        <div className="modal-footer">
-                            <button className="secondary-button" onClick={handleClose}>
-                                Close
-                            </button>
-                            <button className="primary-button" onClick={handleSuccess}>
-                                Save Changes
-                            </button>
-                        </div>
-
-                    </div>
-                </Modal>
-
+                </div>
             </div>
         </div>
     );
---
+++ src/src/images.js
@@ -0,0 +1,49 @@
+import Outlook from './img/Outlook.svg';
+import Confluence from './img/Confluence.svg';
+import ChatGPT from './img/ChatGPT.png';
+import Jira from './img/Jira.png';
+import WebStorm from './img/WebStorm.png';
+import PhpStorm from './img/PhpStorm.png';
+import Diary from './img/Diary.webp';
+import Bitbucket from './img/Bitbucket.svg';
+import KeePassXC from './img/KeePassXC.png';
+import Firefox from './img/Firefox.png';
+import Mousepad from './img/Mousepad.png';
+import Zoom from './img/Zoom.svg';
+import Postman from './img/Postman.svg';
+import Without from './img/Without.jpg';
+import YouTube from './img/YouTube.png';
+import Calculator from './img/Calculator.png';
+import Tinder from './img/Tinder.svg';
+import Github from './img/Github.svg';
+import Plus from './img/Plus.svg';
+import GTranslate from './img/GTranslate.png';
+import Telegram from './img/Telegram.png';
+import Cross from './img/CrossMarkSvgRepoCom.svg';
+import Voice from './img/speaker-wave-2-svgrepo-com.svg';
+
+export {
+    Outlook,
+    Confluence,
+    ChatGPT,
+    Jira,
+    WebStorm,
+    PhpStorm,
+    Diary,
+    Bitbucket,
+    KeePassXC,
+    Firefox,
+    Mousepad,
+    Zoom,
+    Postman,
+    Without,
+    YouTube,
+    Calculator,
+    Tinder,
+    Github,
+    Plus,
+    GTranslate,
+    Telegram,
+    Cross,
+    Voice
+};
