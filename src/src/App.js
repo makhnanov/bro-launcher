424c8046e1779e0516a2de44af2d96219a73c666
Roman Bakirov <bakirov.important@gmail.com>
2023-06-29 01:30:42 +0600

    1.6.16. Hotfix lifetime manager.

--- src/src/App.js
+++ src/src/App.js
@@ -36,7 +36,7 @@ import Tabex from "./widgets/Tabex";
 
 document.title = 'BRO Launcher';
 
-const appVersion = '1.6.15';
+const appVersion = '1.6.16';
 
 function App() {
     const [bookmarks, setBookmarks] = useState(JSON.parse(localStorage.getItem('bookmarks') ?? '[]'));
