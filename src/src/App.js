73bf1c217f9c785782d4bb3b1b4b1208612b5b18
Roman Bakirov <bakirov.important@gmail.com>
2023-07-20 19:57:40 +0600

    Change v

--- src/src/App.js
+++ src/src/App.js
@@ -44,7 +44,7 @@ import PutinFlag from "./img/PutinFlag.png";
 
 document.title = 'BRO Launcher';
 
-const appVersion = '1.7.12';
+const appVersion = '1.7.13';
 
 function App() {
     const [bookmarks, setBookmarks] = useState(JSON.parse(localStorage.getItem('bookmarks') ?? '[]'));
