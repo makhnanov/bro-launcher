52709fe83038436678f2c732d76d8f2db7a4f5af
Roman Bakirov <bakirov.important@gmail.com>
2023-08-03 16:44:43 +0600

    1.7.22. Fix links padding.

--- src/src/components/LinksColumn.js
+++ src/src/components/LinksColumn.js
@@ -5,7 +5,7 @@ const LinksColumn = ({links, noteHidden}) => {
     return (<div className={`links-column`} style={{display: noteHidden ? 'none' : ''}}>
         {links.map((link, index) => (
             <div className={'link-container'} style={{
-                marginTop: ((15.1 * (link.index)) - (15.1 * (links[index - 1]?.index + 1) )) + "px"
+                top: ((15.1 * (link.index))) + "px"
             }}>
                 <a href={link.url} target={"_blank"} className={'link-a'} >
                     <img src={LinkImg} alt={'Link'} className={'link-icon'}/>
