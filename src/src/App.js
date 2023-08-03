fcec49d1a6778cd5e7487385f1d57fec52e2fcaf
Roman Bakirov <bakirov.important@gmail.com>
2023-08-03 21:15:02 +0600

    Upd v

--- src/src/App.js
+++ src/src/App.js
@@ -36,7 +36,7 @@ import Tabex from "./widgets/Tabex";
 
 document.title = 'BRO Launcher';
 
-const appVersion = '1.7.22';
+const appVersion = '1.7.23';
 
 function App() {
     const [bookmarks, setBookmarks] = useState(JSON.parse(localStorage.getItem('bookmarks') ?? '[]'));
