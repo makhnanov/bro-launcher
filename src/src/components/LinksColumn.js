58d5f8541082333284baf30f759fa86178df2967
Roman Bakirov <bakirov.important@gmail.com>
2023-10-03 12:28:31 +0600

    v2.0.2: Fix simple notes links height.

--- src/src/components/LinksColumn.js
+++ src/src/components/LinksColumn.js
@@ -8,35 +8,39 @@ const LinksColumn = ({links, noteHidden}) => {
         width: noteHidden || !links.length ? "0" : "20px",
     }}>
         {links.map((link, index) => (
-            <div key={index} className={'link-container'} style={{
-                top: ((15.1 * (link.index))) + "px",
-            }}>
-                <a href={link.url.split(' ')[0]} target={"_blank"} className={'link-a'} >
-                    <img src={
+            <div
+                key={index}
+                className={'link-container'} style={{
+                    top: ((15.1 * (link.index))) + "px",
+                }}
+                onClick={() => window.open(link.url.split(' ')[0], '_self')}
+                onAuxClick={() => window.open(link.url.split(' ')[0], '_blank')}
+            >
+                <img
+                    src={
                         link.url.toLowerCase().includes('jira')
                             ? Jira
                             : LinkImg
-                    } alt={'Link'}
-                         className={`link-icon ${
-                             link.url.endsWith('red') ?
-                                 'link-icon_red'
-                                    : link.url.endsWith('green')
-                                        ? "link-icon_green"
-                                             : link.url.endsWith('blue')
-                                                 ? "link-icon_blue"
-                                                     : link.url.endsWith('yellow')
-                                                         ? "link-icon_yellow"
-                                                         : ""
-                         }`}
-                         style={{
-                        height: link.url.toLowerCase().includes('jira')
-                            ? "unset"
-                            : "14px",
-                    }}/>
-                </a>
+                    }
+                    alt={'Link'}
+                    className={`link-icon 
+                    ${
+                        link.url.endsWith('red') ?
+                            'link-icon_red'
+                               : link.url.endsWith('green')
+                                   ? "link-icon_green"
+                                        : link.url.endsWith('blue')
+                                            ? "link-icon_blue"
+                                                : link.url.endsWith('yellow')
+                                                    ? "link-icon_yellow"
+                                                    : ""
+                    }`}
+                />
             </div>
         ))}
     </div>)
 }
 
 export default LinksColumn;
+
+
