76b250879ff742e93551883d3130185335a3bef6
Roman Bakirov <bakirov.important@gmail.com>
2023-10-04 21:41:50 +0600

    v2.0.5: Add images loading lazy.

--- src/src/components/Item.js
+++ src/src/components/Item.js
@@ -5,8 +5,11 @@ const Item = ({item}) => {
         <div className={'item-content'}>
             <div className={'item-image-container'}>
                 <div className={`item-img-wrapper ${item.imgStyle}`}>
-                    <img src={item.img} alt="Alt" className={`item-image-icon ${item.imgStyle}`}
-                         draggable={"false"}></img>
+                    <img src={item.img}
+                         alt="Alt"
+                         className={`item-image-icon ${item.imgStyle}`}
+                         draggable={"false"}
+                         loading={"lazy"}></img>
                 </div>
             </div>
             <div className="text-description-block">
